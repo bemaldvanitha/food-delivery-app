84e59d7cc68de70c02d87fe0f7b7537004219659
bemaldvanitha <bdvidanagamachchi@gmail.com>
2020-12-15 08:42:33 +0530

    edit product validation completed

--- screens/EditProductScreen.js
+++ screens/EditProductScreen.js
@@ -18,28 +18,87 @@ const EditProductScreen = (props) => {
     const [description,setDescription] = useState(editProductId ? currentProduct.description : '');
     const [imageUrl,setImageUrl] = useState(editProductId ? currentProduct.imageUrl :
         'https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040_1280.jpg');
-
-    const [halfPotionAvailable,setHalfPortionAvailable] = useState(false);
-    const [halfPortionPrice,setHalfPortionPrice] = useState(editProductId ? currentProduct.fullPortionPrice : '');
-    const [fullPortionPrice,setFullPortionPrice] = useState(editProductId ? currentProduct.halfPortionPrice : '');
-
+    const [halfPotionAvailable,setHalfPortionAvailable] = useState(!!(editProductId && currentProduct.halfPortionPrice !== 0));
+    const [halfPortionPrice,setHalfPortionPrice] = useState(editProductId ? currentProduct.halfPortionPrice.toString() : '');
+    const [fullPortionPrice,setFullPortionPrice] = useState(editProductId ? currentProduct.fullPortionPrice.toString() : '');
     const [isVegan,setIsVegan] = useState(editProductId ? currentProduct.isVegan : false);
     const [isVegetarian,setIsVegetarian] = useState(editProductId ? currentProduct.isVegetarian : false);
     const [isSugarFree,setIsSugarFree] = useState(editProductId ? currentProduct.isSugarFree : false);
     const [categoryId,setCategoryId] = useState(editProductId ?currentProduct.catId : Categories[0].id);
+
+    const [isNameValid,setIsNameValid] = useState(!!editProductId);
+    const [isDescriptionValid,setIsDescriptionValid] = useState(!!editProductId);
+    const [isImageUrlValid,setIsImageUrlValid] = useState(!!editProductId);
+    const [isHalfPortionValid,setIsHalfPortionValid] = useState(!!editProductId);
+    const [isFullPortionValid,setIsFullPortionValid] = useState(!!editProductId);
+
+    const nameValidator = (text) => {
+        if(text.trim().length < 6){
+            setIsNameValid(false);
+        }else{
+            setIsNameValid(true);
+        }
+        setName(text);
+    }
+
+    const descriptionValidator = (text) => {
+        if(text.trim().length < 7){
+            setIsDescriptionValid(false);
+        }else{
+            setIsDescriptionValid(true);
+        }
+        setDescription(text)
+    }
+
+    const imageUrlValidator = (text) => {
+        if(text.trim().length > 7 && (text.includes('http') || text.includes('https') )){
+            setIsImageUrlValid(true);
+        }else{
+            setIsImageUrlValid(false);
+        }
+        setImageUrl(text);
+    }
+
+    const halfPortionValidator = (text) => {
+        if(text.trim().length !== 0 && !isNaN(text)){
+            if(parseFloat(text) > 0){
+                setIsHalfPortionValid(true);
+            }else{
+                setIsHalfPortionValid(false);
+            }
+        }else{
+            setIsHalfPortionValid(false);
+        }
+        setHalfPortionPrice(text);
+    }
+
+    const fullPortionValidator = (text) => {
+        if(text.trim().length !== 0 && !isNaN(text)){
+            if(parseFloat(text) > 0){
+                setIsFullPortionValid(true);
+            }else{
+                setIsFullPortionValid(false);
+            }
+        }else{
+            setIsFullPortionValid(false);
+        }
+        setFullPortionPrice(text);
+    }
 
     return(
         <ScrollView>
             <View style={styles.screen}>
 
                 <View style={styles.inputContainer}>
-                    <TextInput value={name} onChangeText={(text) => setName(text)} keyboardType='default' label='enter name' mode='flat'
+                    <TextInput value={name} onChangeText={nameValidator} keyboardType='default' label='enter name' mode='flat'
                                underlineColor='black' />
+                    {!isNameValid && <Text style={styles.errorText}>enter valid name</Text>}
                 </View>
 
                 <View style={styles.inputContainer}>
-                    <TextInput value={description} onChangeText={(text) => setDescription(text)} keyboardType='default' label='enter description'
+                    <TextInput value={description} onChangeText={descriptionValidator} keyboardType='default' label='enter description'
                                mode='flat' underlineColor='black' multiline={true} numberOfLines={3}/>
+                    {!isDescriptionValid && <Text style={styles.errorText}>enter description</Text>}
                 </View>
 
                 <View style={styles.switchInputContainer}>
@@ -49,20 +108,25 @@ const EditProductScreen = (props) => {
                 </View>
 
                 <View style={styles.inputContainer}>
-                    <TextInput value={fullPortionPrice} onChangeText={(text) => setFullPortionPrice(text)} keyboardType='numeric'
+                    <TextInput value={fullPortionPrice} onChangeText={fullPortionValidator} keyboardType='numeric'
                                label='enter full portion price' mode='flat' underlineColor='black' />
+                    {!isFullPortionValid && <Text style={styles.errorText}>enter valid price</Text>}
                 </View>
 
                 {
                     halfPotionAvailable && <View style={styles.inputContainer}>
-                        <TextInput value={halfPortionPrice} onChangeText={(text) => setHalfPortionPrice(text)}
+                        <TextInput value={halfPortionPrice} onChangeText={halfPortionValidator}
                                    keyboardType='numeric' label='enter half portion price' mode='flat' underlineColor='black' />
+                        {!isHalfPortionValid && <Text style={styles.errorText}>enter valid price</Text>}
                     </View>
                 }
                 <View style={styles.imageUrlContainer}>
                     <Image source={{uri: imageUrl}} style={styles.image}/>
-                    <TextInput value={imageUrl} onChangeText={(text) => setImageUrl(text)} keyboardType='default' label='enter image url'
-                               mode='flat' underlineColor='black' multiline={true} numberOfLines={4} style={styles.imageInput}/>
+                    <View>
+                        <TextInput value={imageUrl} onChangeText={imageUrlValidator} keyboardType='default' label='enter image url'
+                                   mode='flat' underlineColor='black' multiline={true} numberOfLines={4} style={styles.imageInput}/>
+                        {!isImageUrlValid && <Text style={styles.errorText}>enter valid image url</Text>}
+                    </View>
                 </View>
 
                 <Picker style={styles.pickerContainer} mode='dropdown' selectedValue={categoryId} onValueChange={(val) => setCategoryId(val)}>
@@ -137,6 +201,10 @@ const styles = StyleSheet.create({
     },
     pickerContainer: {
         marginHorizontal: screenWidth * 0.04
+    },
+    errorText: {
+        color: Colors.primaryColor,
+        paddingHorizontal: screenWidth * 0.1
     }
 })
 
